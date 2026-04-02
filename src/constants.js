1bd91e83f77f2d02816c77f8a8fb0b9e200f0516
fribah <fridtjof.hungnes@gmail.com>
2026-04-02 22:35:29 +0200

    revamp

--- src/constants.js
+++
@@ -1,4 +0,0 @@
-export const BLUE = "#1D4ED8";
-export const BL = "#EFF4FF";
-export const BM = "#DBEAFE";
-export const FF = "'Manrope',sans-serif";
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// Hide SEO content once React loads
+document.body.classList.add('react-loaded')
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
